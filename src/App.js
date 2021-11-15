5fe9094624bea45d0a4fd43d08afb44e0af1288e
xulihang <xulihanghai@163.com>
2021-11-15 17:05:10 +0800

    add ios;disable button if is scanning; add pwa support

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import { DBR,Options } from 'capacitor-plugin-dynamsoft-barcode-reader';
+import { DBR } from 'capacitor-plugin-dynamsoft-barcode-reader';
 import { Toast } from '@capacitor/toast';
+import { Capacitor } from '@capacitor/core';
 
 function App() {
-    
+
   async function scan() {
+    document.getElementById("scan").setAttribute("disabled","");
     var result = await DBR.scan({"organizationID":"200001"});
-    await Toast.show({
-      text: result.barcodeFormat+": "+result.barcodeText
-    });
+    document.getElementById("scan").removeAttribute("disabled");
+    let message = result.barcodeFormat+": "+result.barcodeText;
+    if (Capacitor.isNativePlatform === true){
+      await Toast.show({
+        text: message
+      });
+    }else{
+      alert(message);
+    }
+    
   }
     
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={scan}>
+        <button id="scan" onClick={scan}>
             Start Scanning
         </button>
       </header>
