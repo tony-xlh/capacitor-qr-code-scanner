cb9751a599cf649f251a6075b626b1a93ceed3ae
xulihang <xulihanghai@163.com>
2021-11-18 10:17:55 +0800

    add scanner control

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import './Scanner.css';
 import { DBR } from 'capacitor-plugin-dynamsoft-barcode-reader';
 import { Toast } from '@capacitor/toast';
 import { Capacitor } from '@capacitor/core';
 
 function App() {
-
+  var torchOn = false;
   async function scan() {
     showMessage("Starting")
     try{
-      var result = await DBR.scan({"organizationID":"200001"});
+      showControlAndhideBackground();
+      var result = await DBR.scan({"organizationID":"100227763"});
+      hideControlAndRevealBackground();
       let message = result.barcodeFormat+": "+result.barcodeText;
       showMessage(message)
     }catch(e){
       alert(e.message);
     }
+  }
+
+  function showControlAndhideBackground(){
+    document.getElementById("scannerControl").style.display="inherit";
+    document.getElementsByClassName("App-header")[0].style.backgroundColor="transparent";
+  }
+
+  function hideControlAndRevealBackground(){
+    document.getElementsByClassName("App-header")[0].style.backgroundColor="#282c34";
+    document.getElementById("scannerControl").style.display="none";
   }
   
   async function showMessage(message){
@@ -27,6 +40,19 @@ function App() {
       alert(message);
     }
   }
+
+  function toggleTorch(){
+    try{
+      let desiredState = true;
+      if (torchOn){
+        desiredState = false;
+      }
+      DBR.toggleTorch({"on":desiredState});
+      torchOn = desiredState;
+    }catch(e){
+      alert(e.message);
+    }
+  }
     
   return (
     <div className="App">
@@ -36,6 +62,11 @@ function App() {
             Start Scanning
         </button>
       </header>
+      <div id="scannerControl" >
+        <button onClick={toggleTorch}>
+            Toggle Torch
+        </button>
+      </div>
     </div>
   );
 }
